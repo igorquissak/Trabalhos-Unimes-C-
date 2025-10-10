78a59935ecb788e8a21ed1ed0b73b839aef94590
Igor <igorquissak1@gmail.com>
2025-10-09 23:04:11 -0300

    Tabuada

---
+++ CalculadoraTabuada.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,num2, c;
+
+            Console.Write("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite até que número quer ver o seu ser multiplicado: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int soma = 0;
+            
+            for (c = 1; c <= num2; c++)
+            {
+
+                
+                soma = soma + num;
+
+                Console.WriteLine(num + "x" + c + "=" + soma);
+                
+                Thread.Sleep(500);
+            }
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
