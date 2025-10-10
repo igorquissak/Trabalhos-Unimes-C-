a601794700cd133089d9fbd2628f60b3b98092f1
Igor <igorquissak1@gmail.com>
2025-10-09 23:03:26 -0300

    Media-for

---
+++ Soma_Media.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            double media;
+            Console.Write("Digite um número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int c = 1; c <=num; c++ )
+            {
+                Console.Write(c + "º: ");
+               
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                soma = soma + num2;
+            }
+            media = soma / num;
+            Console.WriteLine("Soma: " +soma);
+            Console.WriteLine("Media: " + media);
+           
+            Console.ReadKey();
+        }
+    }
+}
